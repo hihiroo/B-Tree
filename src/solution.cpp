e25064de2c183cb1f122b865e23a9c4a80cf9f11
hihiroo <alsrud606@naver.com>
2023-09-08 14:33:15 +0900

    삭제 기능 완성

--- src/solution.cpp
+++ src/solution.cpp
@@ -5,6 +5,37 @@
 #include <algorithm>
 using namespace std;
 
+struct Node{
+    int *keys, *values, numKeys, maxChilds;
+    Node **childs;
+    bool leaf;
+
+    Node(int maxChilds, bool leaf): maxChilds(maxChilds), leaf(leaf){
+        keys = new int[maxChilds-1];
+        values = new int[maxChilds-1];
+        childs = new Node *[maxChilds];
+        numKeys = 0;
+    }
+
+    pair<Node*,int> search(int key);
+    void insertion(int key, int value);
+    void split(int childIdx);
+    void traversal(vector<pair<int,int> >* data);
+    void deletion(int key);
+    void merge(int idx);
+    void makeNonMinimum(int idx);
+
+    pair<int,int> findmin(){
+        if(leaf) return {keys[0], values[0]};
+        return childs[0]->findmin();
+    }
+    pair<int,int> findmax(){
+        if(leaf) return {keys[numKeys-1], values[numKeys-1]};
+        return childs[numKeys]->findmax();
+    }
+};
+
+
 struct BTree{
     int degree;
     Node *root;
@@ -38,62 +69,24 @@ struct BTree{
 
     void deletion(int key){
         if(root == NULL) return;
-            // key 검색
-            // key 있음
-                // leaf 노드일 때
-                    // 그냥 삭제
-                    // key 개수가 0이면 노드 삭제
-                // leaf 노드가 아닐 때 
-                    // 왼쪽 자식의 키가 남을 때
-                        // 왼쪽 서브트리의 max 값 검색 후 돌아옴
-                        // max 값을 key가 있던 자리에 덮어쓰기
-                        // 왼쪽 서브트리에 대해 max 값 삭제 함수 호출
-                    // 오른쪽 자식의 키가 남을 때
-                        // 위와 동일하지만 min 값 검색
-                    // 둘 다 안남을 때
-                        // 왼쪽자식 + key + 오른쪽 자식 merge 후 키 한 칸씩 당김. 
-                        // 키 개수가 0이면 루트를 왼쪽 자식으로 변경하고 현재 루트 삭제
-                        // 왼쪽 서브트리 삭제 함수 호출
-            // key 없음
-                // 왼쪽 자식이 미니멈 노드인 경우
-                    // 왼쪽 자식의 인접 노드들이 키가 남는 경우
-                        // 현재 노드의 key를 왼쪽 자식의 제일 오른쪽에 삽입
-                        // 인접 노드의 키(max or min)를 현재 노드의 key에 덮어쓰기
-                    // 안 남는 경우
-                        // 왼쪽 자식+현재 키+오른쪽 자식 merge, 오른쪽 자식 없애고 현재 노드 key 한 칸씩 당기기
-                        // key 개수가 0이면 루트를 왼쪽 자식으로 변경하고 현재 루트 삭제
-                        // 이미 현재 노드의 부모에서 현재 노드가 미니멈 노드가 아니도록 해놨음
 
+        root->deletion(key);
+
+        if(root->numKeys == 0){
+            Node *tmp = root;
+            if(root->leaf) root = NULL;
+            else root = root->childs[0];
+            delete tmp;
+        }
     }            
 };
-
-
-struct Node{
-    int *keys, *values, numKeys, maxChilds;
-    Node **childs;
-    bool leaf;
-
-    Node(int maxChilds, bool leaf): maxChilds(maxChilds), leaf(leaf){
-        keys = new int[maxChilds-1];
-        values = new int[maxChilds-1];
-        childs = new Node *[maxChilds];
-        numKeys = 0;
-    }
-
-    pair<Node*,int> search(int key);
-    void insertion(int key, int value);
-    void split(int childIdx);
-    void traversal(vector<pair<int,int> >* data);
-    void deletion(int key);
-};
-
 
 /*
     return the node and index of the key
 */
 pair<Node*,int> Node::search(int key){
     int idx = lower_bound(keys, keys+numKeys, key) - keys;
-    if(keys[idx] == key) return {this,idx};
+    if(idx < numKeys && keys[idx] == key) return {this,idx};
     else if(leaf == true) return {NULL,-1};
     else return childs[idx]->search(key);
 }
@@ -172,28 +165,147 @@ void Node::traversal(vector<pair<int,int> >* data){
     if(numKeys > 0 && !leaf) childs[numKeys]->traversal(data);
 }
 
+/*
+    루트 노드를 제외한 노드에 대하여 항상 minimum 노드가 아님을 보장
+*/
 void Node::deletion(int key){
-    // key 검색
-    // key 있음
-        // leaf 노드일 때
-            // 그냥 삭제
-        // leaf 노드가 아닐 때 
-            // 왼쪽 자식의 키가 남을 때
-                // 왼쪽 서브트리의 max 값 검색 후 돌아옴
-                // max 값을 key가 있던 자리에 덮어쓰기
-                // 왼쪽 서브트리에 대해 max 값 삭제 함수 호출
-            // 오른쪽 자식의 키가 남을 때
-                // 위와 동일하지만 min 값 검색
-            // 둘 다 안남을 때
-                // 왼쪽자식 + key + 오른쪽 자식 merge 후 키 한 칸씩 당김. 왼쪽 서브트리 삭제 함수 호출
-    // key 없음
-        // 왼쪽 자식이 미니멈 노드인 경우
-            // 왼쪽 자식의 인접 노드들이 키가 남는 경우
-                // 현재 노드의 key를 왼쪽 자식의 제일 오른쪽에 삽입
-                // 인접 노드의 키(max or min)를 현재 노드의 key에 덮어쓰기
-            // 안 남는 경우
-                // 왼쪽 자식+현재 키+오른쪽 자식 merge, 오른쪽 자식 없애고 현재 노드 key 한 칸씩 당기기
-                // 이미 현재 노드의 부모에서 현재 노드가 미니멈 노드가 아니도록 해놨음
+    int minKeys = maxChilds/2-1; // 최소 키 개수
+    int idx = lower_bound(keys, keys+numKeys, key) - keys; // key 검색
+    
+    if(idx < numKeys && keys[idx] == key){ // key가 현재 노드에 있음
+        if(leaf){ // leaf 노드라면 그냥 key 삭제
+            for(int i=idx; i<numKeys-1; i++){
+                keys[i] = keys[i+1];
+                values[i] = values[i+1];
+            }
+            numKeys--;
+        }
+        else{ // leaf 노드가 아닐 때 
+            if(childs[idx]->numKeys > minKeys){ // 왼쪽 자식이 minimum node가 아니면
+                pair<int,int> leftMax = childs[idx]->findmax(); // 왼쪽 서브트리의 max 값 검색 후 돌아옴
+                keys[idx] = leftMax.first; // max 값을 key가 있던 자리에 덮어쓰기
+                values[idx] = leftMax.second;
+                childs[idx]->deletion(leftMax.first); // 왼쪽 서브트리에 대해 max값 삭제 함수 호출
+            }
+            else if(childs[idx+1]->numKeys > minKeys){ // 오른쪽 자식이 minimum node가 아니면
+                pair<int,int> rightMin = childs[idx+1]->findmin(); // 위와 동일하지만 min 값 검색
+                keys[idx] = rightMin.first;
+                values[idx] = rightMin.second;
+                childs[idx+1]->deletion(rightMin.first);
+            }
+            else{ // 왼쪽 오른쪽 자식이 다 minimum node일 때
+                merge(idx); // key와 오른쪽 자식을 왼쪽 자식에 merge (즉, key를 아래로 내림)
+                childs[idx]->deletion(key);
+            }
+        }
+    }
+    else{ // key가 현재 노드에 없음
+        if(childs[idx]->numKeys == minKeys){ // 왼쪽 자식(childs[idx])이 minimum node인 경우
+            makeNonMinimum(idx);
+            idx = min(idx, numKeys);
+        }
+        childs[idx]->deletion(key);
+    }
+}
+
+
+/*
+    childs[idx]가 minimum node가 아니도록 만드는 함수
+    인접한 형제 노드에서 key를 가져오거나 merge를 통해 full node로 만들 수 있음
+*/
+void Node::makeNonMinimum(int idx){
+    int minKeys = maxChilds/2-1; // 최소 키 개수
+
+    if(idx > 0 && childs[idx-1]->numKeys > minKeys){ // childs[idx-1]이 키가 남는 경우
+        // childs[idx]의 key를 오른쪽으로 한 칸씩 shifting
+        for(int i=minKeys; i>0; i--){
+            childs[idx]->keys[i] = childs[idx]->keys[i-1];
+            childs[idx]->values[i] = childs[idx]->values[i-1];
+        }
+        if(childs[idx]->leaf == false){
+            for(int i=minKeys+1; i>0; i--){
+                childs[idx]->childs[i] = childs[idx]->childs[i-1];
+            }
+        }
+        // keys[idx-1]을 childs[idx]의 가장 왼쪽으로 내리기
+        childs[idx]->keys[0] = keys[idx-1];
+        childs[idx]->values[0] = values[idx-1];
+        childs[idx]->numKeys++;
+
+        // childs[idx-1]의 가장 오른쪽 키를 keys[idx-1]로 올리기
+        int sibling_key_cnt = childs[idx-1]->numKeys;
+        keys[idx-1] = childs[idx-1]->keys[sibling_key_cnt-1];
+        values[idx-1] = childs[idx-1]->values[sibling_key_cnt-1];
+        childs[idx-1]->numKeys--;
+
+        // childs[idx-1]의 가장 오른쪽 자식을 childs[idx]의 가장 왼쪽 자식으로 옮기기
+        if(childs[idx]->leaf == false)
+            childs[idx]->childs[0] = childs[idx-1]->childs[sibling_key_cnt];
+    }
+    else if(idx < numKeys && childs[idx+1]->numKeys > minKeys){ // childs[idx+1]이 키가 남는 경우
+        // keys[idx]를 childs[idx]의 가장 오른쪽으로 내리기
+        childs[idx]->keys[minKeys] = keys[idx]; 
+        childs[idx]->values[minKeys] = values[idx];
+        childs[idx]->numKeys++;
+
+        // childs[idx+1]의 가장 왼쪽 키를 keys[idx]로 올리기
+        keys[idx] = childs[idx+1]->keys[0];
+        values[idx] = childs[idx+1]->values[0];
+        childs[idx+1]->numKeys--;
+
+        // childs[idx+1]의 key를 왼쪽으로 한 칸씩 shifting
+        int sibling_key_cnt = childs[idx+1]->numKeys;
+        for(int i=0; i<sibling_key_cnt; i++){
+            childs[idx+1]->keys[i] = childs[idx+1]->keys[i+1];
+            childs[idx+1]->values[i] = childs[idx+1]->values[i+1];
+        }
+        if(childs[idx]->leaf == false){
+            // childs[idx+1]의 가장 왼쪽 자식을 childs[idx]의 가장 오른쪽 자식으로 옮기기
+            childs[idx]->childs[minKeys+1] = childs[idx+1]->childs[0];
+            for(int i=0; i<=sibling_key_cnt; i++){
+                childs[idx+1]->childs[i] = childs[idx+1]->childs[i+1];
+            }
+        }
+    }
+    else{ // 안 남는 경우
+        if(idx == numKeys) idx--; // 가장 오른쪽 자식인 경우 
+        merge(idx);
+    }
+}
+
+/*
+    keys[idx], 오른쪽 자식을 왼쪽 자식에 순서대로 merge
+*/
+void Node::merge(int idx){
+    int minkey = maxChilds/2 - 1;
+
+    // merge
+    childs[idx]->keys[minkey] = keys[idx];
+    childs[idx]->values[minkey] = values[idx];
+
+    for(int i=0; i<minkey; i++){
+        childs[idx]->keys[minkey+1+i] = childs[idx+1]->keys[i];
+        childs[idx]->values[minkey+1+i] = childs[idx+1]->values[i];
+    }
+    childs[idx]->numKeys = 2*minkey + 1;
+
+    if(childs[idx]->leaf == false){
+        for(int i=0; i<minkey+1; i++){
+            childs[idx]->childs[minkey+1+i] = childs[idx+1]->childs[i];
+        }
+    }
+
+    delete childs[idx+1]; // 오른쪽 자식 삭제
+
+    // key shifting
+    for(int i=idx; i<numKeys-1; i++){
+        keys[i] = keys[i+1];
+        values[i] = values[i+1];
+    }
+    for(int i=idx+1; i<numKeys; i++){
+        childs[i] = childs[i+1];
+    }
+    numKeys--;
 }
 
 
@@ -228,7 +340,12 @@ void parsing(string filename, vector<pair<int,int> >* data){
             cout << "can't find any delimiters in line number " << lineNum << '\n';
             continue;
         }
-        data->emplace_back(stoi(line.substr(0,delimiter)), stoi(line.substr(delimiter+1)));
+        string key = line.substr(0, delimiter);
+        string value = line.substr(delimiter+1);
+        if(value[0] >= '0' && value[0] <= '9') 
+            data->emplace_back(stoi(key), stoi(value));
+        else
+            data->emplace_back(stoi(key), NULL);
     }
     file.close();
     return;
@@ -251,17 +368,16 @@ bool compare(vector<pair<int,int> > *data, vector<pair<int,int> > *res){
     return true;
 }
 
-void insertion(BTree* tree){
+void insertion(BTree* tree, vector<pair<int,int> > *data){
     string filename;
 
     cout << "file path for insertion: ";
     cin >> filename;
 
-    vector<pair<int,int> > data;
-    parsing(filename, &data);
+    parsing(filename, data);
 
     vector<pair<int,int> >::iterator itr;
-    for(itr=data.begin(); itr!=data.end(); itr++){
+    for(itr=data->begin(); itr!=data->end(); itr++){
         tree->insertion(itr->first, itr->second);
     }
     cout << "insertion completed\n";
@@ -269,7 +385,7 @@ void insertion(BTree* tree){
     // search
     ofstream file("searched.csv");
 
-    for(itr=data.begin(); itr!=data.end(); itr++){
+    for(itr=data->begin(); itr!=data->end(); itr++){
         auto ans = tree->search(itr->first);
         if(ans.first == NULL) 
             file << itr->first << " " << "N/A\n";
@@ -286,7 +402,7 @@ void insertion(BTree* tree){
     vector<pair<int,int> > results;
     parsing("searched.csv", &results);
 
-    if(compare(&data, &results)){
+    if(compare(data, &results)){
         cout << filename + " equals searched.csv\n";
     }
     else{
@@ -294,7 +410,7 @@ void insertion(BTree* tree){
     }
 }
 
-void deletion(BTree* tree){
+void deletion(BTree* tree, vector<pair<int,int> > *input){
     string filename;
 
     cout << "file path for deletion: ";
@@ -303,9 +419,46 @@ void deletion(BTree* tree){
     vector<pair<int,int> > data;
     parsing(filename, &data);
 
+    // deletion
     vector<pair<int,int> >::iterator itr;
     for(itr=data.begin(); itr!=data.end(); itr++){
         tree->deletion(itr->first);
+        if(tree->search(itr->first).first != NULL){
+            cout << "failed to delete " << itr->first << '\n';
+        }
+    }
+    cout << "deletion completed\n";
+
+    // search
+    ofstream file("delete_results.csv");
+    for(itr=input->begin(); itr!=input->end(); itr++){
+        auto ans = tree->search(itr->first);
+        if(ans.first == NULL) 
+            file << itr->first << " " << "N/A\n";
+        else{
+            int val = ans.first->values[ans.second];
+            file << itr->first << " " << val << '\n';
+        }
+    }
+    file.close();
+    cout << "saved the result file named 'delete_results.csv'\n";
+
+
+    // compare
+    cout << "file path for compare: ";
+    cin >> filename;
+
+    vector<pair<int,int> > compareData;
+    parsing(filename, &compareData);
+
+    vector<pair<int,int> > results;
+    parsing("delete_results.csv", &results);
+
+    if(compare(&compareData, &results)){
+        cout << filename + " equals delete_results.csv\n";
+    }
+    else{
+        cout << filename + " doesn't equal delete_results.csv\n";
     }
 }
 
@@ -318,6 +471,7 @@ int main(int argc, char *argv[]){
     }
 
     BTree tree(degree);
+    vector<pair<int,int> > data;
 
     while(1){
         int command;
@@ -325,10 +479,10 @@ int main(int argc, char *argv[]){
         cin >> command;
 
         if(command == 1){ // insertion
-            insertion(&tree);
+            insertion(&tree, &data);
         }
         else if(command == 2){ // deletion
-            deletion(&tree);
+            deletion(&tree, &data);
         }
         else return 0;
     }
